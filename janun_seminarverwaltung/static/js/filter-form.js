2d1e024f460ec7bf70fe6d834cb7d89ec52a3b6e
Henrik Kröger <hedwig@janun.de>
2018-09-22 16:41:23 +0200

    dont submit filter on input change, because its confusing

--- janun_seminarverwaltung/static/js/filter-form.js
+++ janun_seminarverwaltung/static/js/filter-form.js
@@ -1,27 +1,11 @@
-var delay = (function(){
-  var timer = 0;
-  return function(callback, ms){
-    clearTimeout (timer);
-    timer = setTimeout(callback, ms);
-  };
-})();
-
 // submits the form on change
 var filterForms = document.getElementsByClassName('filter-form');
 for (var i=0; i<filterForms.length; i++) {
   var form = filterForms[i];
-
   var selects = form.querySelectorAll('select');
   for (var j=0; j<selects.length; j++) {
     selects[j].addEventListener('change', function (e) {
       form.submit();
-    });
-  }
-
-  var inputs = form.querySelectorAll('input, textarea');
-  for (var k=0; k<inputs.length; k++) {
-    inputs[k].addEventListener('input', function (e) {
-      delay(function () { form.submit() }, 500);
     });
   }
 }
