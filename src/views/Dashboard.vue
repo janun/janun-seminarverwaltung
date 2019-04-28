6a3fea5af6d02fd5271289337c4a76c64c9d61a3
Henrik Kröger <hedwig@janun.de>
2019-04-28 17:46:53 +0200

    Rename Dashboard Components

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,17 +1,17 @@
 <script lang="ts">
 import Vue from 'vue';
 import store from '@/store/index.ts';
-import TeamerDashboard from '@/views/TeamerDashboard.vue';
-import StaffDashboard from '@/views/StaffDashboard.vue';
+import DashboardTeamer from '@/views/DashboardTeamer.vue';
+import DashboardStaff from '@/views/DashboardStaff.vue';
 
 export default Vue.extend({
   functional: true,
   render(h) {
     const isStaff = store.getters['auth/isStaff'];
     if (isStaff) {
-      return h(StaffDashboard);
+      return h(DashboardStaff);
     } else {
-      return h(TeamerDashboard);
+      return h(DashboardTeamer);
     }
   }
 });
