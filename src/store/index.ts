c8b3225018274277756e5f344096bafaed5b693c
Henrik Kröger <hedwig@janun.de>
2019-05-01 20:41:32 +0200

    Delete extraneous disabled vuex modules

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,7 +4,6 @@ import auth from './modules/auth';
 import groups from './modules/groups';
 import seminars from './modules/seminars';
 import toasts from './modules/toasts';
-// import alerts from './modules/alerts';
 import users from './modules/users';
 
 Vue.use(Vuex);
@@ -12,7 +11,6 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
     auth,
-    // alerts,
     toasts,
     users,
     groups,
