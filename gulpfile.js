36dad88183b89fccbf02b451a5eeb15cc5dd928c
Henrik Kröger <hedwig@riseup.net>
2019-09-25 02:02:55 +0200

    Add purgecss and minification in gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require("gulp");
+
+gulp.task("styles", function() {
+  const postcss = require("gulp-postcss");
+  return gulp
+    .src("backend/static/styles/base.css")
+    .pipe(postcss())
+    .pipe(gulp.dest("backend/static/css"));
+});
+
+gulp.task("watch", function() {
+  gulp.watch(
+    ["backend/templates/**/*.html", "backend/static/styles/*.css"],
+    gulp.parallel("styles")
+  );
+});
+
+gulp.task("default", gulp.parallel("styles"));
--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,14 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./backend/templates/**/*.html"],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  whitelist: ["button", "input", "optgroup", "select", "textarea"]
+});
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")]
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    purgecss,
+    require("cssnano")
+  ]
 };
