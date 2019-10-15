7134b616b528e1dae0c35c2b2b12b036057c7a2c
Henrik Kröger <hedwig@riseup.net>
2019-10-15 23:59:04 +0200

    Add to access control who may edit a seminar

--- backend/seminars/views.py
+++ backend/seminars/views.py
@@ -54,6 +54,11 @@ class SeminarUpdateView(
     def test_func(self):
         return user_may_access_seminar(self.request.user, self.get_object())
 
+    def form_valid(self, form):
+        if not self.request.user == self.get_object().owner:
+            raise PermissionDenied()
+        return super().form_valid(form)
+
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         kwargs["request"] = self.request
