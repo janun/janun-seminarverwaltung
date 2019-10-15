29ce31ca3a1af9183cac590e6e44d8db3d608129
Henrik Kröger <hedwig@riseup.net>
2019-10-16 00:34:18 +0200

    move tests to folder

--- backend/tests/test_pwned_password_validator.py
+++ backend/tests/test_pwned_password_validator.py
@@ -5,6 +5,9 @@ from backend.validators import PwnedPasswordValidator
 
 
 class PwnedPasswordValidatorTestCase(TestCase):
-    def test_raises(self):
+    def test_bad_password_raises_validation_error(self):
         with self.assertRaises(ValidationError):
             PwnedPasswordValidator().validate("password123456")
+
+    def test_good_password_passes(self):
+        PwnedPasswordValidator().validate("aib3giNgoozae4")
