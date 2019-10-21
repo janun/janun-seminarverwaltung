e1dbd531e7bbdbb91df65107165726ac0c038a99
Henrik Kröger <hedwig@riseup.net>
2019-10-22 00:52:23 +0200

    Use reverse in tests

--- backend/users/tests/tests.py
+++ backend/users/tests/tests.py
@@ -1,9 +1,13 @@
+from django.urls import reverse
 from django.test import TestCase
+
 from backend.users.models import User
 from backend.groups.models import JANUNGroup
 
 
 class LoginTestCase(TestCase):
+    url = reverse("account_login")
+
     @classmethod
     def setUpTestData(cls):
         cls.testuser = User(name="Max Mustermann", username="testuser")
@@ -11,19 +15,19 @@ class LoginTestCase(TestCase):
         cls.testuser.save()
 
     def test_login_get(self):
-        get_response = self.client.get("/accounts/login/")
+        get_response = self.client.get(self.url)
         self.assertContains(get_response, "Anmelden")
 
     def test_login_post_success(self):
         post_response = self.client.post(
-            "/accounts/login/", {"login": "testuser", "password": "secret"}, follow=True
+            self.url, {"login": "testuser", "password": "secret"}, follow=True
         )
-        self.assertRedirects(post_response, "/")
+        self.assertRedirects(post_response, reverse("dashboard"))
         self.assertContains(post_response, "Willkommen, Max Mustermann")
 
     def test_login_post_failure(self):
         post_response = self.client.post(
-            "/accounts/login/", {"login": "foobar", "password": "blablabla"}
+            self.url, {"login": "foobar", "password": "blablabla"}
         )
         self.assertContains(
             post_response, "Der Anmeldename und/oder das Passwort sind leider falsch."
@@ -31,18 +35,20 @@ class LoginTestCase(TestCase):
 
 
 class SingupTestCase(TestCase):
+    url = reverse("account_signup")
+
     @classmethod
     def setUpTestData(cls):
         cls.testgroup = JANUNGroup(name="Test-Gruppe")
         cls.testgroup.save()
 
     def test_signup_get(self):
-        response = self.client.get("/accounts/signup/")
+        response = self.client.get(self.url)
         self.assertContains(response, "Neues Konto Anlegen", status_code=200)
 
     def test_signup_post(self):
         post_response = self.client.post(
-            "/accounts/signup/",
+            self.url,
             {
                 "name": "Max Mustermann",
                 "email": "max@mustermann.com",
