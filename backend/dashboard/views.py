73926a8309d45d6cf37ed136b268cdd4ae1ab41d
Henrik Kröger <hedwig@riseup.net>
2019-10-22 17:38:39 +0200

    Return of the green add seminar button on dash

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -3,13 +3,15 @@ from django.views.generic import TemplateView
 
 class Dashboard(TemplateView):
     template_name = "dashboard/dashboard.html"
-    seminar_limit = 5
+    seminar_limit = 6
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["janun_groups"] = self.request.user.janun_groups.all()
         context["group_hats"] = self.request.user.group_hats.all()
-        context["seminars"] = self.request.user.seminars.all()[: self.seminar_limit]
+        context["seminars"] = self.request.user.seminars.all().order_by("created_at")[
+            : self.seminar_limit
+        ]
         context["show_more_link"] = (
             self.request.user.seminars.count() > self.seminar_limit
         )
