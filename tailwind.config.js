960045c07c7f779a050eae84863ee280d45d0fa8
Henrik Kröger <hedwig@riseup.net>
2019-10-22 15:54:56 +0200

    Darken select arrow icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,9 @@ module.exports = {
         "checkbox, radio": {
           color: theme("colors.gray.700")
         },
+        select: {
+          iconColor: theme('colors.gray.700'),
+        },
         textarea: {
           resize: "none"
         }
