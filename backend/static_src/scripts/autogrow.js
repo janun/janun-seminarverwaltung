18ffe5ee8c9ec44ec5e9bb67ebb4d7ea549f1cbf
Henrik Kröger <hedwig@riseup.net>
2019-10-24 20:34:32 +0200

    Autogrow on page load too

--- backend/static_src/scripts/autogrow.js
+++ backend/static_src/scripts/autogrow.js
@@ -1,7 +1,12 @@
+function autogrow(el) {
+    el.style.setProperty('height', 'auto');
+    var newHeight = el.offsetHeight + el.scrollHeight - el.clientHeight;
+    el.style.height = newHeight + "px";
+}
+
 document.querySelectorAll(".js-autogrow").forEach(function (el) {
     el.addEventListener('input', function (event) {
-        el.style.setProperty('height', 'auto');
-        var newHeight = el.offsetHeight + el.scrollHeight - el.clientHeight;
-        el.style.height = newHeight + "px";
+        autogrow(el);
     });
+    autogrow(el);
 });
