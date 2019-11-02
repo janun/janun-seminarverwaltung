708d7cae94247048475d517e2c544e2e54a94292
Henrik Kröger <hedwig@riseup.net>
2019-11-03 00:26:44 +0100

    Remove created_at

--- backend/seminars/tables.py
+++ backend/seminars/tables.py
@@ -1,5 +1,7 @@
-import django_tables2 as tables
 from django.utils.html import format_html
+from django.template import defaultfilters
+
+import django_tables2 as tables
 
 from backend.utils import NumericColumn, EuroColumn
 
@@ -23,45 +25,35 @@ def render_pill(value, color="green"):
 
 
 class SeminarTable(tables.Table):
-    # checkbox = tables.CheckBoxColumn(
-    #     accessor="pk", attrs={"input": {"class": "form-checkbox"}}
-    # )
     title = tables.Column(linkify=True, attrs={"td": {"class": "primary-column"}})
     start_date = tables.DateColumn(
         verbose_name="Datum", format="d.m.y", attrs={"cell": {"class": "numeric"}}
     )
     owner = tables.Column(verbose_name="Besitzer_in", linkify=True)
     group = tables.Column(verbose_name="Gruppe", linkify=True)
-    # training_days = tables.Column(
-    #     verbose_name="B-Tage", attrs={"cell": {"class": "numeric"}}
-    # )
-    # attendees = tables.Column(verbose_name="TN", attrs={"cell": {"class": "numeric"}})
     tnt = NumericColumn(verbose_name="TNT")
     funding = EuroColumn(verbose_name="Förderung")
     tnt_cost = EuroColumn(verbose_name="€/TNT")
     deadline = tables.DateColumn(
         verbose_name="Deadline", format="d.m.y", attrs={"cell": {"class": "numeric"}}
     )
-    created_at = tables.DateColumn(
-        verbose_name="angemeldet", format="d.m.y", attrs={"cell": {"class": "numeric"}}
-    )
 
     def render_status(self, record, value):
         color = STATE_INFO[value]["color"]
         return render_pill(value, color)
 
-    def render_deadline(self, record):
+    def render_deadline(self, record, value):
         status = record.deadline_status
-        deadline = record.deadline.strftime("%d.%m.%y")
+        formatted = defaultfilters.date(value, "d.m.Y")
         if status == "not_applicable":
             return "—"
         if status == "not_soon":
-            return deadline
+            return formatted
         if status == "expired":
             color = "red"
         elif status == "soon":
             color = "yellow"
-        return render_pill(deadline, color)
+        return render_pill(formatted, color)
 
     class Meta:
         model = Seminar
