21b9c089ec493c8b2e087e4ea48f9c28279275d6
Henrik Kröger <hedwig@riseup.net>
2019-11-12 19:33:55 +0100

    scrollspy rewrite which actually works
    including a fix for the last element

--- backend/static_src/scripts/scroll-spy.js
+++ backend/static_src/scripts/scroll-spy.js
@@ -1,39 +1,47 @@
 document.querySelectorAll('.js-scroll-spy').forEach(function (container) {
-  var sectionOffsets = {}
-  container.querySelectorAll('.js-scroll-spy-section').forEach(function (section) {
-    sectionOffsets[section.id] = section.offsetTop;
-  })
+  var sections = container.querySelectorAll('.js-scroll-spy-section');
 
   var menu = container.querySelector('.js-scroll-spy-menu');
-  var activeClass = 'js-scroll-spy-active';
-  var activeClasses = menu.getAttribute('data-js-scroll-spy-class').split(' ');
-  var offset = menu.getAttribute('data-offset');
+  var menuItems = menu.querySelectorAll("a")
+  var classlist = menu.getAttribute('data-js-scroll-spy-class').split(' ');
+  var offset = menu.getAttribute('data-offset') || 0;
 
   function onScroll(event) {
-    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-    scrollPosition = parseInt(scrollPosition);
-    if (offset) {
-      scrollPosition -= offset;
+    activeSections = [];
+    sections.forEach(function (section) {
+      if (section.getBoundingClientRect().top < offset) {
+        activeSections.push(section)
+      }
+    });
+
+    menuItems.forEach(function (menuItem) {
+      classlist.forEach(function (klass) {
+        menuItem.classList.remove(klass);
+      })
+    });
+
+    if (activeSections.length > 0) {
+      var activeSection = activeSections[activeSections.length - 1];
+      var activeMenuItem = menu.querySelector('[href="#' + activeSection.id + '"]');
+
+      classlist.forEach(function (klass) {
+        activeMenuItem.classList.add(klass);
+      });
     }
 
-    for (i in sectionOffsets) {
-      if (sectionOffsets[i] <= scrollPosition) {
-        var oldActive = menu.querySelector('.' + activeClass)
-        if (oldActive) {
-          oldActive.classList.remove(activeClass);
-          activeClasses.forEach(function (klass) {
-            oldActive.classList.remove(klass);
-          });
-        }
-        var newActive = menu.querySelector('a[href*=' + i + ']');
-        if (newActive) {
-          newActive.classList.add(activeClass);
-          activeClasses.forEach(function (klass) {
-            newActive.classList.add(klass);
-          });
-        }
-      }
+    // Fix for last element if scrolled to the bottom
+    if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+      menuItems.forEach(function (menuItem) {
+        classlist.forEach(function (klass) {
+          menuItem.classList.remove(klass);
+        })
+      });
+
+      classlist.forEach(function (klass) {
+        menuItems[menuItems.length - 1].classList.add(klass);
+      });
     }
+
   }
 
   document.addEventListener('scroll', onScroll);
