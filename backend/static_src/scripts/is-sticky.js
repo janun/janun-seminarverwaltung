f667ee272dd3f3e53f0a048939e45e0abbc3c084
Henrik Kröger <hedwig@riseup.net>
2019-11-15 15:19:43 +0100

    Improve js-is-sticky-b using intersection observer

--- backend/static_src/scripts/is-sticky.js
+++ backend/static_src/scripts/is-sticky.js
@@ -3,20 +3,23 @@
 // and adds/removes the classes from data-sticky-classes attribute
 // works for bottom: 0
 document.querySelectorAll('.js-is-sticky-b').forEach(function (elem) {
-    classlist = elem.getAttribute('data-sticky-classes').split(' ');
+    var classlist = elem.getAttribute('data-sticky-classes').split(' ');
 
-    function detectSticky() {
-        if (window.innerHeight - elem.getBoundingClientRect().bottom <= 0) {
+    var observer = new IntersectionObserver(function (entries) {
+        if (entries[0].intersectionRatio === 0)
             classlist.forEach(function (klass) {
                 elem.classList.add(klass);
             });
-        } else {
+        else if (entries[0].intersectionRatio === 1)
             classlist.forEach(function (klass) {
                 elem.classList.remove(klass);
             });
-        }
-    }
+    }, { threshold: [0, 1] });
 
-    document.addEventListener('scroll', detectSticky);
-    detectSticky();
+    // put sth after elem
+    var sentinel = document.createElement('span');
+    elem.insertAdjacentElement('afterend', sentinel);
+
+    // observe
+    observer.observe(sentinel);
 })
