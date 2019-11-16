0e657e793cca22c600f46ad955816f8feef433fa
Henrik Kröger <hedwig@riseup.net>
2019-11-16 17:36:17 +0100

    Fail gracefully if no observer and fix margin-b

--- backend/static_src/scripts/is-sticky.js
+++ backend/static_src/scripts/is-sticky.js
@@ -5,21 +5,29 @@
 document.querySelectorAll('.js-is-sticky-b').forEach(function (elem) {
     var classlist = elem.getAttribute('data-sticky-classes').split(' ');
 
-    var observer = new IntersectionObserver(function (entries) {
-        if (entries[0].intersectionRatio === 0)
-            classlist.forEach(function (klass) {
-                elem.classList.add(klass);
-            });
-        else if (entries[0].intersectionRatio === 1)
-            classlist.forEach(function (klass) {
-                elem.classList.remove(klass);
-            });
-    }, { threshold: [0, 1] });
+    if ('IntersectionObserver' in window) {
+        var observer = new IntersectionObserver(function (entries) {
+            if (entries[0].intersectionRatio === 0)
+                classlist.forEach(function (klass) {
+                    elem.classList.add(klass);
+                });
+            else if (entries[0].intersectionRatio === 1)
+                classlist.forEach(function (klass) {
+                    elem.classList.remove(klass);
+                });
+        }, { threshold: [0, 1] });
 
-    // put sth after elem
-    var sentinel = document.createElement('span');
-    elem.insertAdjacentElement('afterend', sentinel);
+        // put sth after elem
+        var sentinel = document.createElement('div');
+        sentinel.innerHTML = '&nbsp;'
+        var style = elem.currentStyle || window.getComputedStyle(elem);
+        if (style.marginBottom) {
+            sentinel.style.marginTop = "-" + style.marginBottom;
+            sentinel.style.marginBottom = style.marginBottom;
+        }
+        elem.insertAdjacentElement('afterend', sentinel);
 
-    // observe
-    observer.observe(sentinel);
+        // observe
+        observer.observe(sentinel);
+    }
 })
