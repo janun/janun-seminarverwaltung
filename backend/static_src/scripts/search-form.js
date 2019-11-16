803889f75256a885b5d9cb8b5c085224603b7be5
Henrik Kröger <hedwig@riseup.net>
2019-11-16 16:46:25 +0100

    Fix search-form.js for ie

--- backend/static_src/scripts/search-form.js
+++ backend/static_src/scripts/search-form.js
@@ -2,47 +2,57 @@ document.querySelectorAll('.js-search-form').forEach(function (form) {
   var input = form.querySelector('input');
   var results = form.querySelector('.js-search-form-results');
 
+  // focus on Ctrl+Shift+F
   document.addEventListener('keydown', function (event) {
-    if (event.ctrlKey && event.shiftKey && event.code === 'KeyF') {
+    if (event.ctrlKey && event.shiftKey && event.key.toLowerCase() === 'f') {
       input.focus();
     }
   })
 
+  // show results when focus
   input.addEventListener('focus', function (event) {
     results.classList.remove('hidden');
   })
 
+  // select first result on ArrowDown
   input.addEventListener('keydown', function (event) {
-    if (event.code !== 'ArrowDown') return;
-    event.preventDefault();
-    results.querySelectorAll('a')[0].focus()
-  })
-  results.addEventListener('keydown', function (event) {
-    if (event.code !== 'ArrowDown') return;
-    event.preventDefault();
-    var nextSibling = document.activeElement.nextElementSibling;
-    if (nextSibling) nextSibling.focus();
-  })
-  results.addEventListener('keydown', function (event) {
-    if (event.code !== 'ArrowUp') return;
-    event.preventDefault();
-    var prevSibling = document.activeElement.previousElementSibling;
-    if (prevSibling) prevSibling.focus();
-    else input.focus();
+    if (event.key === 'ArrowDown' || event.key == 'Down') {
+      event.preventDefault();
+      results.querySelectorAll('a')[0].focus()
+    }
   })
 
+  // cycle results and inputs on ArrowDown / ArrowUp
+  results.addEventListener('keydown', function (event) {
+    if (event.key === 'ArrowDown' || event.key == 'Down') {
+      event.preventDefault();
+      var nextSibling = document.activeElement.nextElementSibling;
+      if (nextSibling) nextSibling.focus();
+    }
+  })
+  results.addEventListener('keydown', function (event) {
+    if (event.key === 'ArrowUp' || event.key == 'Up') {
+      event.preventDefault();
+      var prevSibling = document.activeElement.previousElementSibling;
+      if (prevSibling) prevSibling.focus();
+      else input.focus();
+    }
+  })
+
+  // hide results on focusout or Escape
   form.addEventListener('focusout', function (event) {
     if (!form.contains(event.relatedTarget))
       results.classList.add('hidden');
   })
-
   form.addEventListener('keydown', function (event) {
-    if (event.code !== 'Escape') return;
-    event.preventDefault();
-    input.blur();
-    results.classList.add('hidden');
+    if (event.key === 'Escape' || event.key === 'Esc') {
+      event.preventDefault();
+      input.blur();
+      results.classList.add('hidden');
+    }
   })
 
+  // start the search on input
   input.addEventListener('input', function (event) {
     if (input.value.length < 3) {
       results.innerHTML = "";
