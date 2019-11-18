02c919d7cccb7e1180b70814d4c20aad04f83d92
Henrik Kröger <hedwig@riseup.net>
2019-11-18 21:41:38 +0100

    show no of new seminars and users in navbar

--- backend/seminars/templatetags/janun.py
+++ backend/seminars/templatetags/janun.py
@@ -4,8 +4,20 @@ from django import forms
 import django_filters
 
 from backend.utils import format_number
+from backend.users.models import User
+from backend.seminars.models import Seminar
 
 register = template.Library()
+
+
+@register.simple_tag()
+def get_unreviewed_users():
+    return User.objects.filter(is_reviewed=False).count()
+
+
+@register.simple_tag()
+def get_new_seminars():
+    return Seminar.objects.filter(status="angemeldet").count()
 
 
 @register.filter
