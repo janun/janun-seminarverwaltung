cd9676e03b66f394235709edec0ad53ff4fa823c
Henrik Kröger <hedwig@riseup.net>
2020-03-03 14:36:26 +0100

    Do not warn of lost changes if submitting

--- backend/static_src/scripts/warn-if-unsaved.js
+++ backend/static_src/scripts/warn-if-unsaved.js
@@ -1,17 +1,21 @@
 /* eslint-disable compat/compat */
+// displays a warning dialog if the user leaves the page when there are unsaved changes
 document.querySelectorAll('.js-warn-if-unsaved').forEach(function (form) {
   if (!URLSearchParams) return
-  var oldData = new URLSearchParams(new FormData(form)).toString()
-  var beforeunload = function (event) {
-    event.returnValue = 'Es gibt ungespeicherte Änderungen in einem Formular.'
-  }
+  var submitted = false
+  var initialData = new URLSearchParams(new FormData(form)).toString()
 
-  form.addEventListener('input', function () {
-    var newData = new URLSearchParams(new FormData(form)).toString()
-    if (oldData !== newData) {
-      window.addEventListener('beforeunload', beforeunload)
-    } else {
-      window.removeEventListener('beforeunload', beforeunload)
+  var beforeunload = function (event) {
+    if (submitted) return
+    var currentData = new URLSearchParams(new FormData(form)).toString()
+    if (initialData !== currentData) {
+      event.preventDefault();
+      event.returnValue = 'Es gibt ungespeicherte Änderungen.'
     }
+  }
+  window.addEventListener('beforeunload', beforeunload)
+
+  form.addEventListener('submit', function () {
+    submitted = true
   })
 })
