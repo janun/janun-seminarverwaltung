5db9744497bcee64867290f5b184e957d299b871
Henrik Kröger <hedwig@riseup.net>
2020-03-27 15:29:30 +0100

    refactor email template code
    exposing get_mails and render

--- backend/emails/models.py
+++ backend/emails/models.py
@@ -1,9 +1,11 @@
 import os
+from typing import Iterator
 
 from django.db import models
 from django.urls import reverse
 from django.template import Template, Context
 from django.core.mail import EmailMessage
+from django.core import mail
 
 from model_utils import Choices
 
@@ -51,22 +53,14 @@ class EmailTemplate(models.Model):
     def get_absolute_url(self) -> str:
         return reverse("emails:update", kwargs={"pk": self.pk})
 
-    @classmethod
-    def _send(cls, template_obj, context):
-        if template_obj.active is False:
-            return
-
-        # evaluate conditions
-        for condition in template_obj.conditions.all():
-            if not condition.evaluate(context):
-                return
-
+    def render(self, context, connection=None) -> EmailMessage:
+        """render this email template using context"""
         # render templates
-        from_email = render_template(template_obj.from_template, context)
-        to_email = render_template(template_obj.to_template, context)
-        cc = render_template(template_obj.cc_template, context)
-        subject = render_template(template_obj.subject_template, context)
-        body = render_template(template_obj.text_template, context)
+        from_email = render_template(self.from_template, context)
+        to_email = render_template(self.to_template, context)
+        cc = render_template(self.cc_template, context)
+        subject = render_template(self.subject_template, context)
+        body = render_template(self.text_template, context)
 
         # create mail
         email = EmailMessage(
@@ -75,15 +69,15 @@ class EmailTemplate(models.Model):
             from_email=from_email,
             to=to_email.split(","),
             cc=cc.split(","),
+            connection=connection,
         )
 
         # attachments
-        for attachment in template_obj.attachments.all():
+        for attachment in self.attachments.all():
             filename = os.path.basename(attachment.file.name)
             email.attach(filename=filename, content=attachment.file.read())
 
-        # actually send the mail
-        email.send()
+        return email
 
     @classmethod
     def send(cls, template_key: str, context: dict):
@@ -92,9 +86,31 @@ class EmailTemplate(models.Model):
         Args:
             template_key: The name of the template
             context: Context to render the template with"""
-        qs = cls.objects.filter(template_key=template_key)
-        for template_obj in qs:
-            cls._send(template_obj, context)
+        for email in cls.get_mails(template_key, context):
+            email.send(fail_silently=False)
+
+    @classmethod
+    def get_mails(cls, template_key: str, context: dict) -> Iterator[EmailMessage]:
+        """Generate mails using template
+
+        Only gets mails for active templates and also evaluates conditions
+        Creates one smtp connection for all found emails
+
+        Args:
+            template_key: The name of the template
+            context: Context to render the template with
+        Returns:
+            Generator of EmailMessages
+            """
+        qs = cls.objects.filter(
+            template_key=template_key, active=True
+        ).prefetch_related("conditions")
+        with mail.get_connection() as connection:
+            for template_obj in qs:
+                if all(
+                    cond.evaluate(context) for cond in template_obj.conditions.all()
+                ):
+                    yield template_obj.render(context, connection=connection)
 
     class Meta:
         verbose_name = "E-Mail-Vorlage"
@@ -121,6 +137,8 @@ class EmailTemplateCondition(models.Model):
     )
 
     def evaluate(self, context: dict) -> bool:
-        expression = "{% if " + self.expression + " %}True{%endif%}"
-        result = render_template(expression, context) == "True"
-        return result
+        expression = "{% if " + self.expression + " %}True{% endif %}"
+        return render_template(expression, context) == "True"
+
+    def __str__(self) -> str:
+        return self.expression
