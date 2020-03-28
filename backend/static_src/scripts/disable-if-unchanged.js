f911e5a5b96c4621454107a89849001bf8a67aee
Henrik Kröger <hedwig@riseup.net>
2020-03-28 01:20:10 +0100

    simplify disable-if-unchaged
    removing the tooltip listing changed fields
    fixing checkbox change detection

--- backend/static_src/scripts/disable-if-unchanged.js
+++ backend/static_src/scripts/disable-if-unchanged.js
@@ -1,41 +1,4 @@
 /* eslint-disable es/no-for-of-loops */
-// get changed fields between two FormData objects
-// doesnt work in ie
-function getChangedFields(oldData, newData) {
-  var fields = []
-  var oldKeys = Array.from(oldData.keys())
-  var newKeys = Array.from(newData.keys())
-
-  for (var key of oldKeys) {
-    var oldValue = oldData.get(key)
-    var newValue = newData.get(key)
-
-    // field was removed
-    if (!newKeys.includes(key)) {
-      fields.push(key)
-    }
-
-    // compare uploads by size
-    if (oldValue instanceof File) {
-      if (oldValue.size != newValue.size) fields.push(key)
-    }
-    else if (oldValue !== newValue) fields.push(key)
-  }
-
-  // check for added keys
-  for (var key2 of newKeys) {
-    if (!oldKeys.includes(key2)) {
-      fields.push(key2)
-    }
-  }
-
-  return fields
-}
-
-function getLabel(form, fieldName) {
-  var field = form.querySelector('[name="' + fieldName + '"]')
-  return field.labels[0].innerText
-}
 
 // surround the button with a span
 function addButtonContainer(button) {
@@ -61,17 +24,15 @@ function removeButtonContainer(button) {
 document.querySelectorAll('.js-disable-if-unchanged').forEach(function (button) {
   var form = button.form
   var oldData = new FormData(form)
+  var oldDataString = new URLSearchParams(oldData).toString()
 
   function updateState() {
     var newData = new FormData(form)
-    var changedFields = getChangedFields(oldData, newData)
+    var newDataString = new URLSearchParams(newData).toString()
 
-    if (changedFields.length > 0) {
+    if (newDataString != oldDataString) {
       button.removeAttribute('disabled')
       removeButtonContainer(button)
-      var labels = changedFields.map(function (field) { return getLabel(form, field) })
-      var title = "Änderungen an " + labels.join(', ') + " speichern."
-      button.setAttribute('title', title)
     } else {
       button.setAttribute('disabled', true)
       button.removeAttribute('title')
